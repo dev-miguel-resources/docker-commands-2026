1b3becca0f249f63a62211d130446cd4d50e370f
dev-miguel-resources <ingenieromiguelch@gmail.com>
2026-09-23 15:55:15 -0300

    class 07: working with secrets, healtcheck and depends on

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Backend funcionando...')
+})
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' })
+})
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000')  
+})
